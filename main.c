2815192b5e1db69c77d38dabbab085c42e1b90f7
Soren-Thumann <sorenthumann@ku.edu>
2026-09-03 16:19:57 -0500

    hello-world
    Add hello-world program

---
+++ main.c
@@ -0,0 +1,10 @@
+//Simple hello world program.
+
+#include <stdio.h>  //Includes the stdio library
+
+int main()  //Initializes main
+{
+    printf("hello world");  //Prints string "hello world"
+
+    return 0;   //Ends main
+}
