cf1762ca538e7854a40a87b411c77ec7a4c5cb02
Soren-Thumann <sorenthumann@ku.edu>
2026-09-03 16:42:14 -0500

    Change "hello" to "bye."

--- hello-world.c
+++ hello-world.c
@@ -1,10 +1,10 @@
-//Simple hello-world program
+//Simple hello-world program. Edited to bye world.
 
 #include <stdio.h>  //Includes the stdio library
 
 int main()  //Initializes main
 {
-    printf("hello world");  //Prints string "hello world"
+    printf("bye world");  //Prints string "bye world"
 
     return 0;   //Ends main
 }
